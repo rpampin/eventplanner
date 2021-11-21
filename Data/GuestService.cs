770c59c45ef34f53b0994ec1be2fdb63c4178fff
Rodrigo <rodrigo.pampin@gmail.com>
2021-11-21 13:21:17 -0300

    Guests

---
+++ Data/GuestService.cs
@@ -0,0 +1,46 @@
+﻿using EventPlanner.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlanner.Data
+{
+    public class GuestService
+    {
+        private readonly AppDBContext _context;
+
+        public GuestService(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Guest>> GetAll(Guid eventId)
+            => await _context.Guests
+            .Where(e => e.Event.Id.Equals(eventId))
+            .OrderBy(e => e.LastName.ToLower())
+            .OrderBy(e => e.Name.ToLower())
+            .ToListAsync();
+
+        public async Task<Guest> GetOne(Guid id)
+            => await _context.Guests.FirstOrDefaultAsync(t => t.Id.Equals(id));
+
+        public async Task<bool> InsertOne(Guest guest)
+        {
+            await _context.Guests.AddAsync(guest);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> UpdateOne(Guest guest)
+        {
+            _context.Guests.Update(guest);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteOne(Guest guest)
+        {
+            _context.Remove(guest);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
